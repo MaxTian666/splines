10178896e6eb38e224d8522da0418651dd8cad02
MaxTian666 <409855255@qq.com>
2018-12-27 23:30:31 +0800

    三次样条 求导及积分

--- splinerstest.py
+++ splinerstest.py
@@ -1,5 +1,6 @@
 from scipy.interpolate import interp1d
 from scipy.interpolate import griddata
+
 import numpy as np 
 import matplotlib.pyplot as plt 
 
@@ -47,6 +48,48 @@ plt.title('Cubic')
 
 plt.gcf().set_size_inches(6,6)
 
+
+#三次样条插值
+from scipy import interpolate
+x = np.arange(0,2*np.pi+np.pi/4,2*np.pi/8)
+y = np.sin(x)
+
+tck = interpolate.splrep(x,y,s=0)
+xnew = np.arange(0,2*np.pi,np.pi/50)
+ynew = interpolate.splev(xnew,tck,der=0)
+
+plt.figure(3)
+plt.subplot(311)
+plt.plot(x,y,'x',xnew,ynew,xnew,np.sin(xnew),x,y,'b')
+plt.legend(['Linear','Cubic Spline','True'])
+plt.axis([-0.05,6.33,-1.05,1.05])
+plt.title("Cubic-spline interpolation")
+
+
+#样条求导
+yder = interpolate.splev(xnew,tck,der=1)
+plt.subplot(312)
+plt.plot(xnew,yder,xnew,np.cos(xnew),'*')
+plt.legend(['Cubic Spline','True'])
+plt.axis([-0.05,6.33,-1.05,1.05])
+plt.title("Derivative estimation from spline")
+
+#样条积分
+def integ(x,tck,constant=-1):
+    x = np.atleast_1d(x)
+    out = np.zeros(x.shape,dtype=x.dtype)
+    for n in range(len(out)):
+        out[n] = interpolate.splint(0,x[n],tck)   #求取任意两点间的积分
+    out += constant
+    return out
+
+
+yint = integ(xnew,tck)
+plt.subplot(313)
+plt.plot(xnew,yint,xnew,-np.cos(xnew),'--')
+plt.legend('Cubic Spline','True')
+plt.axis([-0.05,6.33,-1.05,1.05])
+plt.title("Integral estimation from spline")
 
 plt.show()
 
